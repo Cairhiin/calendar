1348688906ad05cb40f2200b908caea9bff9fce3
Frank van de Voorde <daemonaz@kymp.net>
2023-11-11 13:12:50 +0200

    Fix the month view

--- src/components/Calendar/MonthView.vue
+++ src/components/Calendar/MonthView.vue
@@ -3,8 +3,12 @@
         <div v-for="day in week" class="text-center capitalize flex-1 bg-stone-200 p-2 border-r border-stone-100">{{
             getDayOfTheWeek(day, 'nl-NL')
         }}</div>
-        <div v-for="day in month" class="bg-stone-100 border-stone-200 h-32"
-            :class="{ 'border-r': day % 7 !== 0, 'border-b': day <= 35 }">{{ getDate(day) }}
+        <div v-for="day in month" class="bg-stone-100 border-stone-200 h-32 font-bold text-right px-2 py-1" :class="{
+            'border-r': day % 7 !== 0,
+            'border-b': day <= 35,
+            'text-stone-400 bg-stone-300': getMonth(day) !== calendarDate.getMonth(),
+            'text-stone-900': getMonth(day) === calendarDate.getMonth()
+        }">{{ getDate(day) }}
         </div>
     </div>
 </template>
@@ -22,19 +26,19 @@ export default {
     },
     methods: {
         getDayOfTheWeek(day, locale) {
-
-            const dayOfTheWeek = this.calendarDate.getDay();
-            const startOfTheMonth = new Date(this.calendarDate.getFullYear(), this.calendarDate.getMonth());
-            //start.setDate(startOfTheMonth.getDate() - dayOfTheWeek);
-            //console.log(dayOfTheWeek, startOfTheMonth)
-            //console.log(new Date(date.setDate(this.calendarDate.getDate() - firstDayOfMonth + day)));
             return (new Date(0, 0, day - 1)).toLocaleDateString(locale, { weekday: 'short' })
         },
         getShortDate: (date, locale) =>
             date.toLocaleDateString(locale, { day: 'numeric' }),
         getDate(day) {
             const currentDate = new Date(this.calendarDate);
-            return this.getShortDate(new Date(currentDate.setDate(currentDate.getDate() - currentDate.getDay() + day - 1)));
+            const startOfTheMonth = new Date(this.calendarDate.getFullYear(), this.calendarDate.getMonth()).getDay();
+            return this.getShortDate(new Date(currentDate.setDate(1 - startOfTheMonth + day - 1)));
+        },
+        getMonth(day) {
+            const currentDate = new Date(this.calendarDate);
+            const startOfTheMonth = new Date(this.calendarDate.getFullYear(), this.calendarDate.getMonth()).getDay();
+            return new Date(currentDate.setDate(1 - startOfTheMonth + day - 1)).getMonth();
         }
     }
 }
